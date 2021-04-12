7e48b47e042435aebefd96a9eb3f818535e75831
Chaika <chaikaes@gmail.com>
2021-04-12 16:15:12 +0300

    часть 3

--- src/main/java/ru/geekbrains/lesson2/HomeWork2.java
+++ src/main/java/ru/geekbrains/lesson2/HomeWork2.java
@@ -3,17 +3,22 @@ package ru.geekbrains.lesson2;
 public class HomeWork2 {
 
     public static void main(String[] args){
-        twoNumbers();
-        positiveNegative();
+        twoNumbers(6, 2);
+        twoNumbers(-2, 7);
+        positiveNegative(15);
+        checkNumber(-5);
+        wordsNumber("Hello!", 7);
     }
 
     public static boolean twoNumbers(int a, int b) {
         int sum = a + b;
-        if (sum >= 10 && sum <= 20) {
-            return true;
-        } else {
-            return false;
-        }
+        System.out.println(a > 0);
+        return sum >= 10 && sum <= 20;
+//        if (sum >= 10 && sum <= 20) {
+//            return true;
+//        } else {
+//            return false;
+//        }
     }
 
     public static void positiveNegative(int d) {
@@ -24,4 +29,18 @@ public class HomeWork2 {
         }
     }
 
+    public static boolean checkNumber(int j) {
+        return j < 0;
+//        if (j < 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+    }
+
+    public static void wordsNumber(String f, int h) {
+        for (int counter = 1; counter <= h; counter++) {
+            System.out.println(f);
+        }
+    }
 }
