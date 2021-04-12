8c45dac1afcb237feb6d54291ffda5ab445343c6
Chaika <chaikaes@gmail.com>
2021-04-12 13:00:38 +0300

    что-то сделанное 2

--- src/main/java/ru/geekbrains/lesson2/HomeWork2.java
+++ src/main/java/ru/geekbrains/lesson2/HomeWork2.java
@@ -1,6 +1,27 @@
 package ru.geekbrains.lesson2;
 
 public class HomeWork2 {
-    public static void main(String[] args){}
+
+    public static void main(String[] args){
+        twoNumbers();
+        positiveNegative();
+    }
+
+    public static boolean twoNumbers(int a, int b) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void positiveNegative(int d) {
+        if (d >= 0) {
+            System.out.println("Число положительное: " + d);
+        } else {
+            System.out.println("Число отрицательное: " + d);
+        }
+    }
 
 }
