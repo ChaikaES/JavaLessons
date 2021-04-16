9ec6b434973b235fb90dad52a95f80e170498d35
Chaika <chaikaes@gmail.com>
2021-04-16 12:00:11 +0300

    Второе и третье задания

--- src/main/java/ru/geekbrains/lesson3/HomeWork3.java
+++ src/main/java/ru/geekbrains/lesson3/HomeWork3.java
@@ -6,11 +6,22 @@ import java.util.Scanner;
 public class HomeWork3 {
 
     public static void main(String[] args) {
+        System.out.println("Task_1");
         int[] my_arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         printArray(my_arr);
         changeNumber(my_arr);
         printArray(my_arr);
 
+        System.out.println("Task_2");
+        int[] big_arr = new int[100];
+        fillArray(big_arr);
+        printArray(big_arr);
+
+        System.out.println("Task_3");
+        int[] inc_arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        printArray(inc_arr);
+        increaseNumber(inc_arr);
+        printArray(inc_arr);
     }
 
     public static void printArray(int[] pr) {
@@ -30,5 +41,22 @@ public class HomeWork3 {
         }
     }
 
+    public static void fillArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+    }
+
+    public static void increaseNumber(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<6) {
+                arr[i] *= 2;
+            }
+        }
+
+    }
+
 
 }
+
+
