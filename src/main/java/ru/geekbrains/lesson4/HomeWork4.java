5061076f01e3a0ce71b8e232e7a800b3da085240
Chaika <chaikaes@gmail.com>
2021-04-19 16:31:03 +0300

    Листинг с разбором

--- src/main/java/ru/geekbrains/lesson4/HomeWork4.java
+++ src/main/java/ru/geekbrains/lesson4/HomeWork4.java
@@ -10,11 +10,12 @@ public class HomeWork4 {
     public static final char DOT_X = 'X';
     public static final char DOT_O = 'O';
     public static char[][] map;
-    public static Scanner sc = new Scanner(System.in);
-    public static Random rand = new Random();
+    public static Scanner sc = new Scanner(System.in);//ход человека
+    public static Random rand = new Random();//генератор случайных чисел для компа
 
+    //основной цикл игры
     public static void main(String[] args) {
-        initMap();
+        initMap();//тест программы.Произойдёт инициализация и вывод поля в консоль. Программа завершит свою работу.
         printMap();
         while (true) {
             humanTurn();
@@ -41,6 +42,7 @@ public class HomeWork4 {
         System.out.println("Игра закончена");
     }
 
+    //проверка победы
     public static boolean checkWin(char symb) {
         if (map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return true;
         if (map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return true;
@@ -62,10 +64,11 @@ public class HomeWork4 {
         return true;
     }
 
+    //ход компа
     public static void aiTurn() {
         int x, y;
         do {
-            x = rand.nextInt(SIZE);
+            x = rand.nextInt(SIZE);//rand.nextInt(n) возвращает случайное число в диапазоне от 0 до n – 1 включительно
             y = rand.nextInt(SIZE);
         } while (!isCellValid(x, y));
         System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
@@ -76,18 +79,21 @@ public class HomeWork4 {
         int x, y;
         do {
             System.out.println("Введите координаты в формате X Y");
-            x = sc.nextInt() - 1;
+            x = sc.nextInt() - 1; //запросить координаты и уменьшить их на 1, так как для игрока удобно вводить координаты в формате 1-3
             y = sc.nextInt() - 1;
         } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
-        map[y][x] = DOT_X;
+        map[y][x] = DOT_X;  // можно ли поставить фишку в указанную ячейку. Если ячейка свободна и существует, то поставить крестик
     }
 
+    //проверка ячеек. возможность установить фишки в указанную ячейку
     public static boolean isCellValid(int x, int y) {
         if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
         if (map[y][x] == DOT_EMPTY) return true;
         return false;
     }
 
+    //инициализация поля.заполнить все ячейки.
+    // двойной цикл «пробегает» по всем ячейкам массива и в каждую ячейку записывает символ DOT_EMPTY
     public static void initMap() {
         map = new char[SIZE][SIZE];
         for (int i = 0; i < SIZE; i++) {
@@ -97,15 +103,16 @@ public class HomeWork4 {
         }
     }
 
+    // вывод поля в консоль
     public static void printMap() {
-        for (int i = 0; i <= SIZE; i++) {
+        for (int i = 0; i <= SIZE; i++) { //отвечает за распечатку шапки игрового поля
             System.out.print(i + " ");
         }
         System.out.println();
         for (int i = 0; i < SIZE; i++) {
             System.out.print((i + 1) + " ");
-            for (int j = 0; j < SIZE; j++) {
-                System.out.print(map[i][j] + " ");
+            for (int j = 0; j < SIZE; j++) {// j цикл отвечает за печать одной строки
+                System.out.print(map[i][j] + " ");//счетчик i увеличивается и производится печать следующих строк
             }
             System.out.println();
         }
