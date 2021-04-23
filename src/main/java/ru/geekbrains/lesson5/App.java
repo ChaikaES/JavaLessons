2a013f194e2a875b3daf9c00bd7541e62af6246b
Chaika <chaikaes@gmail.com>
2021-04-23 16:36:48 +0300

    Первый этап

---
+++ src/main/java/ru/geekbrains/lesson5/App.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.lesson5;
+
+public class App {
+    public static void main(String[] args) {
+        Employee stuff = new Employee(
+                "Nikolaev Roman",
+                "manager",
+                "nikolaev@mail.ru",
+                "8-800-123-56-78",
+                24700,
+                37
+        );
+
+    }
+}
---
+++ src/main/java/ru/geekbrains/lesson5/Employee.java
@@ -0,0 +1,25 @@
+package ru.geekbrains.lesson5;
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
+
+    public Employee(String fullName, String position, String email, String phoneNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void printInfo() {
+        System.out.println(fullName);
+    }
+
+
+}
