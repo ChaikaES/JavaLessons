d10351d5e39de72fa09ef9f244fc58151d827889
Chaika <chaikaes@gmail.com>
2021-04-29 20:43:29 +0300

    Two

--- src/main/java/ru/geekbrains/lesson7/Cat.java
+++ src/main/java/ru/geekbrains/lesson7/Cat.java
@@ -3,10 +3,12 @@ package ru.geekbrains.lesson7;
 public class Cat {
     private String name;
     private int appetite;
+//    private boolean full;
 
     public Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
+//        this.full = full;
     }
 
     public void eat(Plate p) {
--- src/main/java/ru/geekbrains/lesson7/Plate.java
+++ src/main/java/ru/geekbrains/lesson7/Plate.java
@@ -3,12 +3,17 @@ package ru.geekbrains.lesson7;
 public class Plate {
     private int food;
 
-        public Plate(int food) {
+    public Plate(int food) {
         this.food = food;
     }
 
     public void decreaseFood(int amount) {
-        food -= amount;
+        if(food < amount){
+            System.out.println("В тарелке меньше еды, чем нужно");
+        }
+        else {
+            food -= amount;
+        }
     }
 
     public void info() {System.out.println("Food amount: " + food);}
